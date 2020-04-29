f6fbbbe0497ed1a1f3b8699cea1f980db4c243df
Moazam05 <salmanmoazam08@gmail.com>
2020-04-30 00:26:17 +0500

    Install React Navigation Packages & Adding Context

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+import { BlogProvider } from "./src/context/BlogContext";
+
+function IndexScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Index Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Blog" component={IndexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
+
+// export default App;
